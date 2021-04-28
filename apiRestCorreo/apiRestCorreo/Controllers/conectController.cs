12bdd107b4ad185b10ee9b73e94db48afff592cc
Your Name <you@example.com>
2021-04-27 22:03:05 -0600

    Add

---
+++ apiRestCorreo/apiRestCorreo/Controllers/conectController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+namespace apiRestCorreo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+   
+    public class conectController : ControllerBase
+    {
+        private string conection = @"Server=bwajkqhku6unxpau1riz-mysql.services.clever-cloud.com; Database=bwajkqhku6unxpau1riz; Uid=ufd0ibkz6bmalqof; Pwd=7koeQc4MmQxje5BCizTd";
+        [HttpGet]
+        public ActionResult Get()
+        {
+            IEnumerable<models.dataUsser> lst = null;
+            using (var db = new MySqlConnection(conection))
+            {
+                var sql = "CALL  spAllUser";
+                lst = db.Query<models.dataUsser>(sql);
+            }
+            return Ok(lst);
+
+        }
+ 
+        [HttpPost]
+        public ActionResult Insert(models.newUsuario models){
+            int result = 0;
+            using (var db = new MySqlConnection(conection))
+            {
+                var sql = "CALL newUserSys ?, ?, ?, ?, ?, ?, ?, ?";
+                result = db.Execute(sql, models);
+            }
+            return Ok(result);
+
+        }
+
+    }
+}
---
+++ apiRestCorreo/apiRestCorreo/Controllers/models/dataUsser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiRestCorreo.Controllers.models
+{
+    //ESTRUCTURA USADA PARA MOSTRAR USUARIOS
+    public class dataUsser
+    {
+        public string nom_usuario { get; set; }
+        public string ape_usuario { get; set; }
+        public string name_email { get; set; }
+        public string telefono { get; set; }
+        public string codigo { get; set; }
+        public string es_activo { get; set; }
+        public string es_admin { get; set; }
+        public string fecha_creacion { get; set; }
+
+    }      
+
+    //STRUCTURA PARA AGREGAR NUEVOS USUARIOS AL SISTEMA
+    public class newUsuario
+    {
+        public string nom_usuario { get; set; }
+        public string ape_usuario { get; set; }
+        public string name_email { get; set; }
+        public string pass_email { get; set; }
+        public string telefono { get; set; }
+        public string codigo { get; set; }
+        public string es_activo { get; set; }
+        public string es_admin { get; set; }
+
+    }
+
+}
